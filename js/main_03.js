62bf1228a00111366786555ef220595e197d65c2
catarinasjpereira90 <catarinasjpereira90@gmail.com>
2021-08-27 00:36:34 +0100

    20210817

--- js/main_03.js
+++ js/main_03.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var mainImg = $('.img-big').attr('src');//variável global para ter src imagem grande
-    $('.image-list li img').click(function(){
+    $('.image-list li img').click(function () {
         var clickImg = $(this).attr('src');//variável para guardar src da imagem clicada
         mainImg = $('.img-big').attr('src');//actualiza a variável da imagem grande 
-        $('.img-big').attr('src',clickImg);//actualiza o src da imagem grande 
-        $(this).attr('src',mainImg);//coloca img grande no tumbnail
+        $('.img-big').attr('src', clickImg);//actualiza o src da imagem grande 
+        $(this).attr('src', mainImg);//coloca img grande no tumbnail
     });
 })
