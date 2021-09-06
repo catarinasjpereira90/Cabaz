c94384f4cf8d4b4c9903fdb08109c235bcec380a
catarinasjpereira90 <catarinasjpereira90@gmail.com>
2021-09-06 20:33:52 +0100

    rev06

--- js/main_02.js
+++ js/main_02.js
@@ -1,9 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.multiple-items').slick({
-  prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
-  nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
+    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
   });
+
+  $('.mobile-items').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 512,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      }
+    ]
+  });
+
+
 })
-
-
-
